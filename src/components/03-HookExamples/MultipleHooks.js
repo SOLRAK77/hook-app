e1ab9376af24cba08ffef1f873b28ab1803ebaf2
SOLRAK77 <ccastillo.perez77@gmail.com>
2021-03-14 17:59:25 -0600

    uso useFetch, parte 2 creando un hookCustom

--- src/components/03-HookExamples/MultipleHooks.js
+++ src/components/03-HookExamples/MultipleHooks.js
@@ -1,18 +1,55 @@
 import React from 'react'
 import { useFetch } from '../../Hooks/useFetch'
+import { useCustomCouter } from '../../Hooks/useCustomCouter'
 
 import './effects.css'
 
 export const MultipleHooks = () => {
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
 
-    console.log(state)
+    const {counter, increment} = useCustomCouter(1);
+
+    //const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
+    const {loading, data, error} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+
+    const {author, quote} = !!data && data[0]
+    console.log(author, quote)
 
     return (
         <div>
-            <h1>Custom Hooks !!!</h1>
-            <hr/>           
-        </div>
+            <h1>BreakingBad Quotes</h1>
+            <hr/>   
+                <div className="row">
+                    <div className="col-4">
+                        <button 
+                            className="btn btn-primary"
+                            onClick={ () => increment(1) }>Siguiente Quote</button>
+                    </div>
+                </div>
+                
+
+                {
+                    loading ?
+                    (                        
+                        <div className="alert alert-info text-center animate__animated animate__bounce animate__slower">
+                        Loading...
+                        </div>
+                    )
+                    :
+                    (
+                        <figure className="text-end animate__animated animate__fadeIn 4s">
+                            <blockquote className="blockquote">
+                                <p>{quote}</p>
+                            </blockquote>
+                            <figcaption className="blockquote-footer">
+                                <cite title="Source Title">{author}</cite>
+                            </figcaption>
+                        </figure>
+                    )
+                }
+
+        </div>                
     )
+
+
 }
 
