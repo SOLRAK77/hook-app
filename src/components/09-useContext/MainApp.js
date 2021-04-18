76e99b14c2597d1d40a425fd1f3c39bb63ae37e0
SOLRAK77 <ccastillo.perez77@gmail.com>
2021-04-18 18:07:55 -0500

    uso de user context, parte 1

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,8 +1,19 @@
 import React from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
 
 export const MainApp = () => {
-    return (        
-        <AppRouter />
+
+    const user = {
+        id : 1,
+        codUser : 'ccastillo',
+        nameUser : 'Carlos Castillo',
+        emailUser : 'ccastillo.perez77@gmail.com'
+    }
+
+    return (    
+        <UserContext.Provider value={user}>
+            <AppRouter />
+        </UserContext.Provider>
     )
 }
---
+++ src/components/09-useContext/UserContext.js
@@ -0,0 +1,16 @@
+import { createContext } from 'react'
+/*
+Este en un higer order componente
+Es decir se puede colocar componente hijos y los hijos pueden accesar
+*/
+
+export const UserContext = createContext(null);
+/*
+=> {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+*/
